4d6ffa141918176855921f4e2d6cea9d69b3b7f7
Risto Saarelma <risto.saarelma@iki.fi>
2023-03-30 13:24:13 +0300

    Askama templating

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{prelude::*, stdin};
 
 use serde::{Deserialize, Serialize};
+use askama::Template;
 
 #[derive(Default, Debug, Serialize, Deserialize)]
 #[serde(default)]
@@ -11,31 +12,16 @@ struct LinkData {
     description: String,
 }
 
+#[derive(Template)]
+#[template(path = "links.html")]
+struct Links {
+    links: Vec<(String, LinkData)>
+}
+
 fn main() {
     let mut buffer = String::new();
     stdin().lock().read_to_string(&mut buffer).unwrap();
 
-    let bookmarks: Vec<(String, LinkData)> = idm::from_str(&buffer).expect("Invalid input");
-
-    // TODO 2023-03-30 Choose and use a templating engine for the generated HTML page.
-
-    println!("<ul>");
-    for (name, data) in bookmarks {
-        println!("<li>");
-        println!(
-            "<a href='{}'>{}</a><br/>",
-            data.url,
-            html_escape::encode_safe(&name)
-        );
-        for tag in data.tags {
-            print!("<a class='tag' href='#tags={}'>{}</a>&nbsp;", tag, html_escape::encode_safe(&tag));
-                print!(" ");
-        }
-        println!("<br/>");
-        if !data.description.is_empty() {
-            println!("<p>{}</p>", data.description);
-        }
-        println!("</li>");
-    }
-    println!("</ul>");
+    println!("{}",
+        Links { links: idm::from_str(&buffer).expect("Invalid input") }.render().unwrap());
 }
