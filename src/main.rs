110e5507c5b75a1fd500ea45a43f4cbfac00067f
Risto Saarelma <risto.saarelma@iki.fi>
2023-03-30 14:09:52 +0300

    Working test version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{prelude::*, stdin};
 
-use serde::{Deserialize, Serialize};
 use askama::Template;
+use clap::Parser;
+use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Serialize, Deserialize)]
 #[serde(default)]
@@ -15,13 +17,40 @@ struct LinkData {
 #[derive(Template)]
 #[template(path = "links.html")]
 struct Links {
-    links: Vec<(String, LinkData)>
+    links: IndexMap<String, LinkData>,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Parse input as TOML.
+    #[arg(long)]
+    toml: bool,
 }
 
 fn main() {
+    let args = Args::parse();
+
     let mut buffer = String::new();
     stdin().lock().read_to_string(&mut buffer).unwrap();
 
-    println!("{}",
-        Links { links: idm::from_str(&buffer).expect("Invalid input") }.render().unwrap());
+    if args.toml {
+        println!(
+            "{}",
+            Links {
+                links: toml::from_str(&buffer).expect("Invalid TOML")
+            }
+            .render()
+            .unwrap()
+        );
+    } else {
+        println!(
+            "{}",
+            Links {
+                links: idm::from_str(&buffer).expect("Invalid IDM")
+            }
+            .render()
+            .unwrap()
+        );
+    }
 }
